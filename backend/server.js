6c2c94a7a702a40cf72de7e28bc56a0cbe775926
Ashish Biswas <biswasashish655@gmail.com>
2026-06-10 09:42:28 +0530

    env setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
-import app from './app.js';
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
+import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Rein Oro Express backend listening on http://localhost:${PORT}`);
+ console.log(`Rein Oro Express backend listening on http://localhost:${PORT}`);
 });

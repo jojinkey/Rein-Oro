0ae648e4a29d78d0886c167214358401d220b12d
Ashish Biswas <biswasashish655@gmail.com>
2026-06-13 11:23:07 +0530

    fixed function changes

--- backend/app.js
+++ backend/app.js
@@ -49,7 +49,7 @@ app.get("*", (req, res, next) => {
  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
  res.status(200).json({ message: "Welcome to Rein Oro API" });
 });
 
